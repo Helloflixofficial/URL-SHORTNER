8c782007afc66c51a448a083ad768e990bf474c9
helloflixofficial <shboby71@gmail.com>
2023-09-11 21:59:52 +0530

    23 profile data setup

--- Components/Forms/AccountProfile.tsx
+++ Components/Forms/AccountProfile.tsx
@@ -18,7 +18,6 @@ import {
 import Image from "next/image";
 import { ChangeEvent } from "react";
 import { ValueOf } from "next/dist/shared/lib/constants";
-
 interface props {
   user: {
     id: string;
@@ -39,23 +38,25 @@ export const AccountProfile = ({ user, btnTitle }) => {
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
     },
   });
- 
-  const handleImage = (e:ChangeEvent,fieldChange: (Value: string) => void) => {
-    e.preventDefault();
-  }
 
+  const handleImage = (
+    e: ChangeEvent,
+    fieldChange: (Value: string) => void
+  ) => {
+    e.preventDefault();
+  };
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col justify-start gap-10"
+        className="flex flex-col justify-start gap-2"
       >
         {/* ///////////////Profile_Image///////////////// */}
         <FormField
@@ -63,7 +64,7 @@ export const AccountProfile = ({ user, btnTitle }) => {
           name="profile_photo"
           render={({ field }) => (
             <FormItem className="flex items-center gap-4">
-              <FormLabel className="text-[white] account-form_image-label">
+              <FormLabel className=" account-form_image-label">
                 {field.value ? (
                   <Image
                     src={field.value}
@@ -84,11 +85,13 @@ export const AccountProfile = ({ user, btnTitle }) => {
                 )}
               </FormLabel>
               <FormControl className="flex-1 text-base-semibold text-gray-200">
-                <Input type="file" 
-                accept="image/*"
-                placeholder="Upload image"
-                className="account-form_image-input"
-                onChange={(e) => handleImage(e , field.onChange)}/>
+                <Input
+                  type="file"
+                  accept="image/*"
+                  placeholder="Upload image"
+                  className="account-form_image-input"
+                  onChange={(e) => handleImage(e, field.onChange)}
+                />
               </FormControl>
             </FormItem>
           )}
@@ -98,15 +101,15 @@ export const AccountProfile = ({ user, btnTitle }) => {
           control={form.control}
           name="Name"
           render={({ field }) => (
-            <FormItem className="flex items-center gap-4 w-full">
+            <FormItem className="flex-col gap-3 w-full">
               <FormLabel className="text-base-semibold text-light-2">
-               Name
+                Name
               </FormLabel>
-              <FormControl className="text-base-semibold text-gray-200">
-                <Input 
-                type="text"
-                className="account-form_input no-focus"
-                {...field} 
+              <FormControl>
+                <Input
+                  type="text"
+                  className="account-form_input no-focus"
+                  {...field}
                 />
               </FormControl>
             </FormItem>
@@ -117,15 +120,15 @@ export const AccountProfile = ({ user, btnTitle }) => {
           control={form.control}
           name="Username"
           render={({ field }) => (
-            <FormItem className="flex items-center gap-4 w-full">
+            <FormItem className="flex-col gap-4 w-full">
               <FormLabel className="text-base-semibold text-light-2">
-               Username
+                Username
               </FormLabel>
-              <FormControl className="text-base-semibold text-gray-200">
-                <Input 
-                type="text"
-                className="account-form_input no-focus"
-                {...field} 
+              <FormControl>
+                <Input
+                  type="text"
+                  className="account-form_input no-focus"
+                  {...field}
                 />
               </FormControl>
             </FormItem>
@@ -136,15 +139,15 @@ export const AccountProfile = ({ user, btnTitle }) => {
           control={form.control}
           name="Bio"
           render={({ field }) => (
-            <FormItem className="flex items-center gap-4 w-full">
+            <FormItem className="flex-col gap-4 w-full">
               <FormLabel className="text-base-semibold text-light-2">
-               Bio
+                Bio
               </FormLabel>
-              <FormControl className="text-base-semibold text-gray-200">
-                <Textarea 
+              <FormControl>
+                <Textarea
                   rows={10}
-                className="account-form_input no-focus"
-                {...field} 
+                  className="account-form_input no-focus"
+                  {...field}
                 />
               </FormControl>
             </FormItem>
